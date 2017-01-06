013deef5f320975b69e9011c06c5fb6a497cb771
Gustav <grulfen3@gmail.com>
2017-01-06 14:06:38 +0100

    Changed test structure

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -15,83 +15,70 @@ def test_new_world_is_empty():
     assert len(world) == 0
 
 
-def test_step_empty_world_is_empty():
-    world = gol.World(randomize=False)
-    world.update()
-    assert len(world) == 0
+class TestUpdateCells:
 
-
-@pytest.mark.parametrize("alive_cells, alive",
-                         [(list(combinations(NEIGHBOURS, 2)), 0),
-                          (list(combinations(NEIGHBOURS, 3)), 1),
-                          (list(combinations(NEIGHBOURS, 4)), 0)])
-def test_dead_cell(alive_cells, alive):
-    """
-
-    2 neighbours -> stays dead
-    xx-    ???
-    --- -> ?-?
-    ---    ???
-
-    3 neighbours -> lives
-    xxx    ???
-    --- -> ?x?
-    ---    ???
-
-    4 neighbours -> stays dead
-    xxx    ???
-    --- -> ?-?
-    --x    ???
-
-
-    """
-    for positions in alive_cells:
-        world = gol.World(3, 3, randomize=False)
-        for x, y in positions:
-            world.set_cell((x, y))
+    def test_step_empty_world_is_empty(self):
+        world = gol.World(randomize=False)
         world.update()
-        assert world[(1, 1)] == alive
+        assert len(world) == 0
 
+    @pytest.mark.parametrize("alive_cells, alive",
+                             [(list(combinations(NEIGHBOURS, 2)), 0),
+                              (list(combinations(NEIGHBOURS, 3)), 1),
+                              (list(combinations(NEIGHBOURS, 4)), 0)])
+    def test_dead_cell(self, alive_cells, alive):
+        """
+        ----------------------+------------------------
+        2 neighbours -> dead  | 3 neighbours -> lives
+        xx-    ???            | xxx    ???
+        --- -> ?-?            | --- -> ?x?
+        ---    ???            | ---    ???
+                              |
+        ----------------------+------------------------
+        4 neighbours -> dead  |
+        xxx    ???            |
+        --- -> ?-?            |
+        --x    ???            |
+                              |
+        ----------------------+------------------------
+        """
+        for positions in alive_cells:
+            world = gol.World(3, 3, randomize=False)
+            for x, y in positions:
+                world.set_cell((x, y))
+            world.update()
+            assert world[(1, 1)] == alive
 
-@pytest.mark.parametrize("alive_cells,alive",
-                         [(list(combinations(NEIGHBOURS, 0)), 0),
-                          (list(combinations(NEIGHBOURS, 1)), 0),
-                          (list(combinations(NEIGHBOURS, 2)), 1),
-                          (list(combinations(NEIGHBOURS, 3)), 1),
-                          (list(combinations(NEIGHBOURS, 4)), 0)])
-def test_live_cell(alive_cells, alive):
-    """
-
-    0 neighbours -> dies
-    ---    ???
-    -x- -> ?-?
-    ---    ???
-
-    1 neighbour -> dies
-    ---    ???
-    -x- -> ?-?
-    --x    ???
-
-    2 neighbours -> survives
-    --x    ???
-    -x- -> ?x?
-    --x    ???
-
-    3 neighbours -> survives
-    --x    ???
-    xx- -> ?x?
-    --x    ???
-
-    4 neighbours -> dies
-    --x    ???
-    xx- -> ?x?
-    x-x    ???
-
-    """
-    for positions in alive_cells:
-        world = gol.World(3, 3, randomize=False)
-        world.set_cell((1, 1))
-        for x, y in positions:
-            world.set_cell((x, y))
-        world.update()
-        assert world[(1, 1)] == alive
+    @pytest.mark.parametrize("alive_cells,alive",
+                             [(list(combinations(NEIGHBOURS, 0)), 0),
+                              (list(combinations(NEIGHBOURS, 1)), 0),
+                              (list(combinations(NEIGHBOURS, 2)), 1),
+                              (list(combinations(NEIGHBOURS, 3)), 1),
+                              (list(combinations(NEIGHBOURS, 4)), 0)])
+    def test_live_cell(self, alive_cells, alive):
+        """
+        ----------------------+------------------------
+        0 neighbours -> dies  |  1 neighbour -> dies
+        ---    ???            |  ---    ???
+        -x- -> ?-?            |  -x- -> ?-?
+        ---    ???            |  --x    ???
+                              |
+        ----------------------+------------------------
+        2 neighbours -> lives |  3 neighbours -> lives
+        --x    ???            |  --x    ???
+        -x- -> ?x?            |  xx- -> ?x?
+        --x    ???            |  --x    ???
+        ----------------------+------------------------
+        4 neighbours -> dies  |
+        --x    ???            |
+        xx- -> ?x?            |
+        x-x    ???            |
+        ----------------------+------------------------
+        """
+        for positions in alive_cells:
+            world = gol.World(3, 3, randomize=False)
+            world.set_cell((1, 1))
+            for x, y in positions:
+                world.set_cell((x, y))
+            world.update()
+            assert world[(1, 1)] == alive
